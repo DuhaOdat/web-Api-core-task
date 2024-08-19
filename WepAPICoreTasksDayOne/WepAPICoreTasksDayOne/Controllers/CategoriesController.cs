c21e64b633b1e0d978268651e9ab1f5cab8705f0
DuhaOdat <odatduha@gmail.com>
2024-08-19 17:42:46 +0300

    task one 20-8-2024

---
+++ WepAPICoreTasksDayOne/WepAPICoreTasksDayOne/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WepAPICoreTasksDayOne.Models;
+
+namespace WepAPICoreTasksDayOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+
+        public CategoriesController(MyDbContext db)
+        { 
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        { 
+            var categories = _db.Categories.ToList();
+        
+           return Ok(categories);
+        }
+
+
+        [HttpGet("id")]
+        public IActionResult Get(int id)
+        { 
+           var category = _db.Categories.Where(c=>c.Id==id).FirstOrDefault();
+            return Ok(category);
+        }
+    }
+}
---
+++ WepAPICoreTasksDayOne/WepAPICoreTasksDayOne/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WepAPICoreTasksDayOne.Models;
+
+namespace WepAPICoreTasksDayOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+        public ProductsController(MyDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = _db.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("id")]
+        public IActionResult Get(int id)
+        {
+            var products = _db.Products.Where(p=>p.Id==id).FirstOrDefault();
+            return Ok(products);
+        }
+    }
+}
